4aa43c99a3a9dd0ee8b039ddbdf103ec1d1cc072
HARAYUKA <tukituki442@icloud.com>
2021-09-18 15:19:17 +0900

    schoolとteacherのデモデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,7 +31,34 @@
 #              superior: true)              
              
 SystemAdmin.find_or_create_by(id: 1) do |admin|
-  admin.email = "admin@example.com"
+  admin.email = "system@email.com"
   admin.password = "password"
 end
 
+puts "SystemAdmin Created"
+
+School.create!(school_name: "サンプル")
+
+puts "School Created"
+
+Teacher.create!(teacher_name: "管理職",
+              email: "admin@email.com",
+              password: "password",
+              password_confirmation: "password",
+              admin: true,
+              school_id: 1) 
+
+Teacher.create!(teacher_name: "入力担当者",
+            email: "creator@email.com",
+            password: "password",
+            password_confirmation: "password",
+            creator: true,
+            school_id: 1)
+
+Teacher.create!(teacher_name: "一般",
+            email: "ippan@email.com",
+            password: "password",
+            password_confirmation: "password",
+            school_id: 1)
+
+puts "Teacher Created"
