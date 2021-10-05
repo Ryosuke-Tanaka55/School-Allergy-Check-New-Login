9eaf8c4873f19e6b85045a1f78ae0ade02bd164f
HARAYUKA <tukituki442@icloud.com>
2021-10-05 13:53:22 +0900

    クラス作成&学校一覧・詳細ページ作成

--- app/controllers/system_admins/schools_controller.rb
+++ app/controllers/system_admins/schools_controller.rb
@@ -1,5 +1,6 @@
 class SystemAdmins::SchoolsController < ApplicationController
   before_action :set_school, only: %i[ show edit update destroy ]
+  after_action :set_class, only: :create
 
   def index
     @schools = School.all
@@ -17,10 +18,11 @@ class SystemAdmins::SchoolsController < ApplicationController
   end
 
   def create
+    debugger
     @school = School.new(school_params)
     if @school.save
       flash[:info] = "学校を新規作成しました"
-      redirect_to system_admins_path
+      redirect_to system_admins_school_path(@school)
     else
       flash[:danger] = "作成に失敗しました"
     end
@@ -43,7 +45,58 @@ class SystemAdmins::SchoolsController < ApplicationController
     end
 
     def set_class
-      @classrooms = @school.classrooms.create(class_name)
+      # @classrooms = @school.classrooms.create(class_name)
+      @classrooms = @school.classrooms.create([
+        {class_name: "1-1"},
+        {class_name: "1-2"},
+        {class_name: "1-3"},
+        {class_name: "1-4"},
+        {class_name: "1-5"},
+        {class_name: "1-6"},
+        {class_name: "1-7"},
+        {class_name: "1-8"},
+        {class_name: "2-1"},
+        {class_name: "2-2"},
+        {class_name: "2-3"},
+        {class_name: "2-4"},
+        {class_name: "2-5"},
+        {class_name: "2-6"},
+        {class_name: "2-7"},
+        {class_name: "2-8"},
+        {class_name: "3-1"},
+        {class_name: "3-2"},
+        {class_name: "3-3"},
+        {class_name: "3-4"},
+        {class_name: "3-5"},
+        {class_name: "3-6"},
+        {class_name: "3-7"},
+        {class_name: "3-8"},
+        {class_name: "4-1"},
+        {class_name: "4-2"},
+        {class_name: "4-3"},
+        {class_name: "4-4"},
+        {class_name: "4-5"},
+        {class_name: "4-6"},
+        {class_name: "4-7"},
+        {class_name: "4-8"},
+        {class_name: "5-1"},
+        {class_name: "5-2"},
+        {class_name: "5-3"},
+        {class_name: "5-4"},
+        {class_name: "5-5"},
+        {class_name: "5-6"},
+        {class_name: "5-7"},
+        {class_name: "5-8"},
+        {class_name: "6-1"},
+        {class_name: "6-2"},
+        {class_name: "6-3"},
+        {class_name: "6-4"},
+        {class_name: "6-5"},
+        {class_name: "6-6"},
+        {class_name: "6-7"},
+        {class_name: "6-8"},
+      ])
+
     end
 
 end
