9b5ee296ddf3517356078ffbfef57607398c0e97
RumiNakamura <krw.psh2196@gmail.com>
2021-10-15 12:22:43 +0900

    alergy_checkモデルにバリデーションを追加、日本語化設定追加、登録機能を作成中

--- app/controllers/alergy_checks_controller.rb
+++ app/controllers/alergy_checks_controller.rb
@@ -6,6 +6,15 @@ class AlergyChecksController < ApplicationController
   end
 
   def create
+    student_id = Student.find(params[:alergy_check][:student_id].to_i).id
+    if student_id.present?
+      @alergy_check = AlergyCheck.new(alergy_menu_params.merge(student_id: student_id))
+      if @alergy_check.save
+        flash[:success] = "献立情報を登録しました。"
+      else
+        flash[:danger] = "登録に失敗しました。"
+      end
+    redirect_to creator_teachers_url
   end
 
   def edit
@@ -16,4 +25,9 @@ class AlergyChecksController < ApplicationController
 
   def destroy
   end
+
+  private
+    def alergy_menu_params
+      params.require(:alergy_check).permit(:worked_on, :student_id, :menu, :support, :note)
+    end
 end
--- app/models/alergy_check.rb
+++ app/models/alergy_check.rb
@@ -1,2 +1,6 @@
 class AlergyCheck < ApplicationRecord
+  validates :worked_on, presence: true, null: false
+  validates :note, length: { maximum: 50 }
+  validates :menu, presence: true, length: { maximum: 20 }, null: false
+  validates :support, presence: true, null: false
 end
