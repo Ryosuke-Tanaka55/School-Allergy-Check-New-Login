806da32eb9a2e0190b2442c17e075f2485b35644
RumiNakamura <krw.psh2196@gmail.com>
2021-11-16 12:54:46 +0900

    showとtoday_indexのアレルギー情報取得条件に日付を追加

--- app/controllers/alergy_checks_controller.rb
+++ app/controllers/alergy_checks_controller.rb
@@ -2,17 +2,17 @@ class AlergyChecksController < ApplicationController
   before_action :set_classroom, only: [:show, :today_index]
 
   def show
-    @alergy_check_sum = @classroom.alergy_checks.all.count
+    @alergy_check_sum = @classroom.alergy_checks.all.where(worked_on: Date.current).count
     #.allと.where(status: "")の数の差分がゼロの時、「本日のチェックは完了しました」と表示する
   end
 
   def today_index
-    @alergy_checks = @classroom.alergy_checks.all
+    @alergy_checks = @classroom.alergy_checks.all.where(worked_on: Date.current) #本日の日付のものに限る
     @teachers = Teacher.where.not(creator: true) #対応法担当者は除く
   end
 
   def update
-    @alergy_check
+    @alergy_check = AlergyCheck.find(params[:alergy_check][:alergy_check_id])
   end
 
   def one_month_index
@@ -21,5 +21,9 @@ class AlergyChecksController < ApplicationController
   private
     def set_classroom
       @classroom = Classroom.find(current_teacher.classroom_id)
+    end
+
+    def today_check_params
+      
     end
 end
