53ef7be954bb0935fa65a3c23ebfda2110d2a7c0
RumiNakamura <krw.psh2196@gmail.com>
2021-11-17 22:56:56 +0900

    update処理とストロングパラメータを追加、_today_indexのビューを編集

--- app/controllers/alergy_checks_controller.rb
+++ app/controllers/alergy_checks_controller.rb
@@ -2,17 +2,23 @@ class AlergyChecksController < ApplicationController
   before_action :set_classroom, only: [:show, :today_index]
 
   def show
-    @alergy_check_sum = @classroom.alergy_checks.all.where(worked_on: Date.current).count
+    @alergy_check_sum = @classroom.alergy_checks.where(worked_on: Date.current).count
     #.allと.where(status: "")の数の差分がゼロの時、「本日のチェックは完了しました」と表示する
   end
 
   def today_index
-    @alergy_checks = @classroom.alergy_checks.all.where(worked_on: Date.current) #本日の日付のものに限る
+    @alergy_checks = @classroom.alergy_checks.where(worked_on: Date.current) #本日の日付のものに限る
     @teachers = Teacher.where.not(creator: true) #対応法担当者は除く
   end
 
   def update
     @alergy_check = AlergyCheck.find(params[:alergy_check][:alergy_check_id])
+    if @alergy_check.update_attributes(today_check_params)
+      flash[:success] = "#{@alergy_check.student.student_name}のチェックを報告しました。"
+    else
+      flash[:danger] = "チェックの報告に失敗しました。入力項目を確認してください。"
+    end
+    redirect_to teachers_alergy_checks_url
   end
 
   def one_month_index
@@ -24,6 +30,6 @@ class AlergyChecksController < ApplicationController
     end
 
     def today_check_params
-      
+      params.require(:alergy_check).permit(:first_check, :second_check, :student_check, :applicant_id).merge(status: "報告中")
     end
 end
