287a56220507494da442da123bcf30ba3e092b52
Leeo-pro <mineva.aug@gmail.com>
2021-11-17 21:54:45 +0900

    管理者ページ-2021117-2

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211019124554) do
+ActiveRecord::Schema.define(version: 20211117122629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admin_alergy_checks", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "alergy_checks", force: :cascade do |t|
     t.date "worked_on", null: false
