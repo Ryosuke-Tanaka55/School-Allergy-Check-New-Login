0d4c4ffe9f6b5620acf7a85b079e3671a5811168
HARAYUKA <tukituki442@icloud.com>
2021-12-14 21:51:34 +0900

    ヘッダーのアプリ名のリンクの条件分岐

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,19 @@
 class StaticPagesController < ApplicationController
+  before_action :teacher_not_show
 
   def top
   end
+
+
+
+  private
+    # 職員がシステム管理者TOPページに遷移できないようにする
+    def teacher_not_show
+      if teacher_signed_in?
+        flash[:danger] = "表示権限がありません。"
+        redirect_to show_teachers_path
+      end
+    end
+
+
 end
