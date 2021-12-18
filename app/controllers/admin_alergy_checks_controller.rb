db70dff5f3c98b5a5d43be936224c574572ef69e
Leeo-pro <mineva.aug@gmail.com>
2021-12-18 20:23:24 +0900

    管理職-申請確認モーダル-20211218

--- app/controllers/admin_alergy_checks_controller.rb
+++ app/controllers/admin_alergy_checks_controller.rb
@@ -2,6 +2,7 @@ class AdminAlergyChecksController < ApplicationController
     include SchoolsHelper
     UPDATE_ERROR_MSG = "登録に失敗しました。やり直してください。"
     before_action :set_first_last_day
+    before_action :admin_teacher, only: [:show]
 
     def one_month_index
       @alergy_checks = AlergyCheck.joins({student: {classroom: :school}})
@@ -19,12 +20,15 @@ class AdminAlergyChecksController < ApplicationController
       @un_checks = Attendance.where(worked_on: @first_day..@last_day).where.not(status: "報告中").where.not(status: "確認済").where.not(status: "要確認").order(:worked_on, :user_id)
     end  
     
-    def lunch_check_info 
-      #debugger
-      #@teacher = Teacher.find(params[:id])
-      ###@requesters = AlergyCheck.joins(:classroom).where(worked_on: Date.today, status: "報告中").group_by(&:class_name)
-      ##requesters = Classroom.includes(:alergy_checks).where(alergy_checks: {worked_on: Date.today, status: "報告中"}).group_by(&:class_name)
-      @requesters = AlergyCheck.where(worked_on: Date.today).group_by(&:applicant)
+    def lunch_check_info
+      #near3 @requesters = AlergyCheck.joins(:classroom).where(worked_on: Date.today).group("classrooms.class_name")
+      #near2 requesters = Classroom.includes(:alergy_checks).where(alergy_checks: {worked_on: Date.today, status: "報告中"}).group_by(&:class_name)
+      #near1 @requesters = AlergyCheck.where(worked_on: Date.today).includes(:classroom).group("classroom.class_name")
+      #original  @requesters = AlergyCheck.where(worked_on: Date.today).group_by(&:applicant)
+      @requesters =  Classroom.includes(students: :alergy_checks).where(alergy_checks: {worked_on: Date.today}).select("alergy_checks.*", "students.*").group_by(&:id)
+      #syota-san1  @requesters = AlergyCheck.joins(:student).where(worked_on: Date.today).group("students.classroom_id").maximum(:id).values
+      #syota-san2  @requesters = AlergyCheck.joins(:classroom).where(worked_on: Date.today).select("alergy_checks.*, classrooms.*").group_by{|record|record.classroom_id}
+      #morimoto @requesters = Classroom.joins(:alergy_checks).merge(AlergyCheck.today)
       #@requesters = Classroom.joins(:alergy_checks).group("class_name").order("count_all DESC").count 
     end 
     
@@ -32,7 +36,7 @@ class AdminAlergyChecksController < ApplicationController
       @user = current_teacher #Teacher.find(params[:id])
       ActiveRecord::Base.transaction do 
        lunch_check_info_params.each do |id, item|
-         if item['status_checker'] == "1"
+         if item['status_checker'] == "1" && item['status'] == "確認済"
            attendance = AlergyCheck.find(id)
            attendance.admin_name = @user.teacher_name
            attendance.update_attributes!(admin_name: @user.teacher_name)
@@ -40,7 +44,7 @@ class AdminAlergyChecksController < ApplicationController
             @info_sum = AlergyCheck.where(worked_on: Date.today, status: "確認済").count
             @unapproval_info_sum = AlergyCheck.where(worked_on: Date.today, status: "要再確認").count
             flash[:success] = "確認済#{@info_sum}件、要再確認#{@unapproval_info_sum}件"
-         end #if end 
+          end #if end 
        end #each end 
        redirect_to teachers_admin_alergy_checks_url
      end #Acctive do end    
--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -4,4 +4,9 @@ class Classroom < ApplicationRecord
   has_many :students, dependent: :destroy
   accepts_nested_attributes_for :students, allow_destroy: true
   has_many :alergy_checks, through: :students
+
+  #森本検証用のため
+  scope :joins_get_all_columns, ->(*tables) {
+    joins(*tables).select("*")
+  }
 end
