474e8fe9c904a7dde9005134f494a19f524511c3
HARAYUKA <tukituki442@icloud.com>
2021-12-22 23:11:36 +0900

    teacherパスワード再設定機能実装途中

--- app/controllers/teachers/passwords_controller.rb
+++ app/controllers/teachers/passwords_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Teachers::PasswordsController < Devise::PasswordsController
+  before_action :set_school
+
   # GET /resource/password/new
   # def new
   #   super
@@ -22,6 +24,12 @@ class Teachers::PasswordsController < Devise::PasswordsController
   # end
 
   # protected
+
+  # schoolの特定
+  def set_school
+    @school = School.find_by!(school_url: params[:school_url])
+  end
+  
 
   # def after_resetting_password_path_for(resource)
   #   super(resource)
